0f58b31288719c9f4a41a8b6565e950d349088b4
Dave <dave.harms@fortyau.com>
2021-10-30 07:08:40 -0500

    added beer client

---
+++ src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,43 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.UUID;
+
+@ConfigurationProperties(value = "sfg.brewery", ignoreUnknownFields = false)
+@Component
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    private String apiHost;
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplate) {
+        this.restTemplate = restTemplate.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apiHost + BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+    }
+
+    public URI saveNewBeer(BeerDto beerDto) {
+        return restTemplate.postForLocation(apiHost + BEER_PATH_V1, beerDto);
+    }
+
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        restTemplate.put(apiHost + BEER_PATH_V1 + beerId, beerDto);
+    }
+
+    public void deleteBeer(UUID beerId) {
+        restTemplate.delete(apiHost + BEER_PATH_V1 + beerId);
+    }
+
+    public void setApiHost(String apiHost) {
+        this.apiHost = apiHost;
+    }
+}
